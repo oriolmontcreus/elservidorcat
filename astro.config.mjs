e6f46dc5132dd4ac8cd2f74c49c082b995d6ea06
oriolmontcreus <licensedgamer641319@gmail.com>
2024-09-22 12:26:43 +0200

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), icon()]
 });
